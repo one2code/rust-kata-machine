8816ce7084bf520c5331ebd3997c9e3359e9e4b6
Travis Bennett <t_bennett_a@protonmail.com>
2023-06-09 23:41:16 -0400

    Boiler plate for linear search - tests

---
+++ src/linearSearchList.rs
@@ -0,0 +1,12 @@
+// Boiler plate code for linear search problem
+
+fn main() {
+    let haystack = [1, 2, 3, 4, 5, 6, 7, 8, 9]: [i32; 9];
+     let needle = 5: i32;
+     
+    
+ 
+     fn linear_search(haystack: &[i32], needle: i32) -> bool {
+        
+     }
+ }
